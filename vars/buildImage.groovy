76d18a491be602cee453eb18622b2cb6612138d0
Aryamaan <153010343+aryamanmaurya@users.noreply.github.com>
2024-10-18 01:14:28 +0530

    Update buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,4 +1,4 @@
-def call(String imageName, String imageTag = "latest" , String dockerFile = "Dockerfile"){
+def call(String imageName, String imageTag = "latest", String dockerFile = "Dockerfile") {
   echo "Building the Docker image: ${imageName}:${imageTag}"
   sh "docker build -t ${imageName}:${imageTag} -f ${dockerfile} ."
   echo "Docker image ${imageName}:${imageTag} built successfully."
